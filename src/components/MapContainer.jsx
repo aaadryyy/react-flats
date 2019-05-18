74d1ab8ef00d4413204cca9f8ea00088d2f0bfd7
Adrien Quillet <adrienquillet@gmail.com>
2019-05-18 21:01:06 +0200

    Constants

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from "./Marker";
+import { API_KEY } from "../constants";
 
 class MapContainer extends Component {
   render() {
     const { center } = this.props.flat;
     const zoom = 13;
-    console.log(center);
 
     return (
       // Important! Always set the container height explicitly
       <div className="map-container">
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAa-AreGMQhpMlVH2qOuZwsWlLRqn0CF5Q" }}
+          bootstrapURLKeys={{ key: API_KEY }}
           center={center}
           zoom={zoom}
         >
